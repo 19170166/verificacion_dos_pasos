d68f539a6640e5d5361674e3f77f0ffc10066bc8
angel zapata <19170166@uttcampus.edu.mx>
2022-03-18 17:49:08 -0600

    feat:auto delete codes

--- app/Http/Controllers/code.php
+++ app/Http/Controllers/code.php
@@ -13,6 +13,7 @@ class code extends Controller
         $code = DB::table('codes')->select('user_code')->where('user_id',$id)->get();
         
         if(Hash::check($request->code,$code[0]->user_code)){
+            DB::delete('delete from codes where user_id = ?', [$id]);
             return redirect('/');
         }else{
             return '<script type="text/javascript">alert("Codigo incorrecto!");</script>';
